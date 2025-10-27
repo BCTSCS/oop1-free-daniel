f91daa3b7caebeef9645b18e04382fd0cd183c1f
Andrew Pan <andrewcypan@gmail.com>
2025-10-27 14:04:15 -0400

    vlogger

---
+++ Vlogger.java
@@ -0,0 +1,16 @@
+public class Vlogger {
+    private String name;
+    private int yearJoined;
+    public Vlogger(){
+        this.name = "AP";
+        this.yearJoined = 2025;
+
+    }
+
+    public String getName(){
+        return name;
+    }
+    public int getYearJoined(){
+        return yearJoined;
+    }
+}
